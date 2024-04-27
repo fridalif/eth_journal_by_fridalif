0cde845a85590dac5ba7cccfa299a9722889f0c3
fridalif <124243435+fridalif@users.noreply.github.com>
2024-04-27 14:02:25 +0300

    models_commit
    Commiting Group, Kid, Subject and Teacher models. Thinking about Many-To-Many raltionship over Teacher and Subject

--- eth_journal/main/models.py
+++ eth_journal/main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Group(models.Model):
+    year_of_study = models.IntegerField(verbose_name='Год набора')
+    group_letter = models.CharField(max_length=2, verbose_name='Буквенный индекс группы')
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+
+class Kid(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь в системе')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name='Группа')
+    father_name = models.TextField(verbose_name='Отчество', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
+
+
+class Subject(models.Model):
+    subject_name = models.TextField(verbose_name='Название предмета')
+
+    class Meta:
+        verbose_name = 'Предмет'
+        verbose_name_plural = 'Предметы'
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь в системе')
+    father_name = models.TextField(verbose_name='Отчество', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Преподаватель'
+        verbose_name_plural = 'Преподаватели'
