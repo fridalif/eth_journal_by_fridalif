5bcbf24a2f383ebb3385a663807e9590489fd66e
fridalif <124243435+fridalif@users.noreply.github.com>
2024-04-28 00:04:27 +0300

    changes in settings and registered models

--- eth_journal/main/admin.py
+++ eth_journal/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Lesson,LessonStudentInfo,Subject,Kid,Teacher,Group
+
 
 # Register your models here.
+admin.site.register(Lesson)
+admin.site.register(LessonStudentInfo)
+admin.site.register(Subject)
+admin.site.register(Kid)
+admin.site.register(Teacher)
+admin.site.register(Group)
