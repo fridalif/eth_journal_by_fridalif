43931e798cb1887578640194e56af35458072312
fridalif <124243435+fridalif@users.noreply.github.com>
2024-04-28 00:46:47 +0300

    created model serializers

--- eth_journal/main/views.py
+++ eth_journal/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse,Http404
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
 def index(request:HttpRequest)->HttpResponse:
     return render(request,'main/index.html')
