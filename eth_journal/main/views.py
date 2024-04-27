38b77a7fad6dbc37ad8f80fcdef960035119805b
fridalif <124243435+fridalif@users.noreply.github.com>
2024-04-28 00:27:19 +0300

    created main/index view and makemigrations

--- eth_journal/main/views.py
+++ eth_journal/main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse,Http404
 
 # Create your views here.
+def index(request:HttpRequest)->HttpResponse:
+    return render(request,'main/index.html')
