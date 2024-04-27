81ca30357241a80f6832fc4d7bbf27d5d397d992
fridalif <124243435+fridalif@users.noreply.github.com>
2024-04-28 01:00:40 +0300

    created simple api

--- eth_journal/main/urls.py
+++ eth_journal/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
-from .views import index
+from .views import index,StudentsAPIView
 
 app_name = 'main'
 urlpatterns = [
-    path('index/',index)
+    path('index/',index),
+    path('api/students/',StudentsAPIView.as_view())
 ]
--- eth_journal/main/views.py
+++ eth_journal/main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpRequest,HttpResponse,Http404
+from django.http import HttpRequest, HttpResponse, Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .serializers import KidSerializer
+from .models import *
+
+
+class StudentsAPIView(APIView):
+    def get(self, request: HttpRequest):
+        return Response(KidSerializer(Kid.objects.all(), many=True).data)
+
+
 # Create your views here.
-def index(request:HttpRequest)->HttpResponse:
-    return render(request,'main/index.html')
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/index.html')
