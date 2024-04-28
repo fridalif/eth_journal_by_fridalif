11008e3919ecaacf7208ac8d2cdc3ce000148426
fridalif <124243435+fridalif@users.noreply.github.com>
2024-04-28 12:37:02 +0300

    rendering auth form

--- eth_journal/main/urls.py
+++ eth_journal/main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
-from .views import index,StudentsAPIView,TestAuthAPIView
+from .views import index,login,StudentsAPIView,TestAuthAPIView
 
 app_name = 'main'
 urlpatterns = [
     path('index/',index),
+    path('login/',login),
     path('api/students/',StudentsAPIView.as_view()),
     path('api/testauth/',TestAuthAPIView.as_view())
 ]
--- eth_journal/main/views.py
+++ eth_journal/main/views.py
@@ -3,19 +3,25 @@ from django.http import HttpRequest, HttpResponse, Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import KidSerializer
+from django.contrib.auth import a
 from .models import *
 
 
 class StudentsAPIView(APIView):
-    def get(self, request: HttpRequest)->Response:
+    def get(self, request: HttpRequest) -> Response:
         return Response(KidSerializer(Kid.objects.all(), many=True).data)
 
+
 class TestAuthAPIView(APIView):
-    def get(self,request: HttpRequest)->Response:
+    def get(self, request: HttpRequest) -> Response:
         auth = request.user.is_authenticated
         print(auth)
-        return Response({'res':auth})
+        return Response({'res': auth})
 
-# Create your views here.
+
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'main/index.html')
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/authentification.html')
