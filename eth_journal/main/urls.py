ee06ee0037eb37a6295ae1df919aff04033e3cc5
fridalif <124243435+fridalif@users.noreply.github.com>
2024-04-30 10:16:10 +0300

    initalized api app

--- eth_journal/main/urls.py
+++ eth_journal/main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import index,register, StudentsAPIView, TestAuthAPIView
+from .views import index,register
 from django.contrib.auth import views
 
 app_name = 'main'
@@ -8,6 +8,4 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(next_page='/index/'), name='logout'),
     path('register/', register, name='register'),
-    path('api/students/', StudentsAPIView.as_view(), name='api_students'),
-    path('api/testauth/', TestAuthAPIView.as_view(), name='api_testauth')
 ]
--- eth_journal/main/views.py
+++ eth_journal/main/views.py
@@ -1,23 +1,11 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse, Http404
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from .serializers import KidSerializer
+#from rest_framework.response import Response
+#from rest_framework.views import APIView
+#from .serializers import KidSerializer
 from .models import *
 from eth_journal.settings import KEY
 from cryptography.fernet import Fernet
-
-
-class StudentsAPIView(APIView):
-    def get(self, request: HttpRequest) -> Response:
-        return Response(KidSerializer(Kid.objects.all(), many=True).data)
-
-
-class TestAuthAPIView(APIView):
-    def get(self, request: HttpRequest) -> Response:
-        auth = request.user.is_authenticated
-        print(auth)
-        return Response({'res': auth})
 
 
 def register(request: HttpRequest) -> HttpResponse:
@@ -32,25 +20,25 @@ def register(request: HttpRequest) -> HttpResponse:
     father_name = request.POST['fathname'].strip()
     role = request.POST['role'].strip()
 
-    #Валидация
+    # Валидация
     if login == '' or password == '' or retype_password == '' or surname == '' or name == '' or role == '':
         context['error'] = 'Пожалуйста, заполните все обязательные поля(отчество, если имеете)'
         return render(request, 'main/register_form.html', context=context)
     if retype_password != password:
         context['error'] = 'Поля Пароль и Повторите пароль должны совпадать'
         return render(request, 'main/register_form.html', context=context)
-    if len(User.objects.filter(username=login))!=0 or len(RegisterRequests.objects.filter(login=login))!=0:
+    if len(User.objects.filter(username=login)) != 0 or len(RegisterRequests.objects.filter(login=login)) != 0:
         context['error'] = 'Пользователь с таким Логином уже существует.'
         return render(request, 'main/register_form.html', context=context)
 
-    #Шифрование пароля(чтобы не хранить в открытом виде в БД) и создание записи в БД
+    # Шифрование пароля(чтобы не хранить в открытом виде в БД) и создание записи в БД
     cipher_suite = Fernet(KEY)
     password = cipher_suite.encrypt(password.encode())
     register_request = RegisterRequests(login=login, password=password, surname=surname, name=name,
                                         father_name=father_name, role=role)
     register_request.save()
     context['error'] = 'Ваш запрос на регистрацию был отправлен на рассмотрение администратору.'
-    return render(request,'main/register_form.html',context=context)
+    return render(request, 'main/register_form.html', context=context)
 
 
 def index(request: HttpRequest) -> HttpResponse:
