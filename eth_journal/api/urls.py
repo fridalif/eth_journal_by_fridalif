34193f011a7498a761bea09f1f08826614b7b19c
fridalif <124243435+fridalif@users.noreply.github.com>
2024-04-30 23:22:06 +0300

    many fixes

--- eth_journal/api/urls.py
+++ eth_journal/api/urls.py
@@ -4,5 +4,6 @@ from .views import RegisterRequestsAPIView, LessonAPIView
 app_name = 'api'
 urlpatterns = [
     path('register_requests/', RegisterRequestsAPIView.as_view(), name='register_requests'),
-    path('lessons/', LessonAPIView.as_view(), name="lessons")
+    path('lessons/', LessonAPIView.as_view(), name="lessons"),
+    path('lessons/<int:lesson_id>/',LessonAPIView.as_view(),name='lesson')
 ]
--- eth_journal/api/views.py
+++ eth_journal/api/views.py
@@ -22,9 +22,10 @@ class RegisterRequestsAPIView(APIView):
         if not data['id']:
             raise Http404
 
-        current_request = main_models.RegisterRequests.objects.get(id=int(data['id']))
-        if current_request is None:
+        current_request = main_models.RegisterRequests.objects.filter(id=int(data['id']))
+        if len(current_request) == 0:
             raise Http404
+        current_request = current_request[0]
 
         cipher_suite = Fernet(KEY)
         password = cipher_suite.decrypt(current_request.password.encode()).decode()
@@ -39,9 +40,11 @@ class RegisterRequestsAPIView(APIView):
             return Response(TeacherSerializer(current_teacher).data)
 
         if data['role'] == 'Student':
-            current_group = main_models.Group.objects.get(id=int(data['group']))
-            if current_group is None:
+            current_group = main_models.Group.objects.filter(id=int(data['group']))
+            if len(current_request) == 0:
                 raise Http404
+            current_group = current_group[0]
+
             current_user = main_models.User(username=current_request.login,
                                             first_name=current_request.name, last_name=current_request.surname)
             current_user.set_password(password)
@@ -61,19 +64,24 @@ class RegisterRequestsAPIView(APIView):
         if not data['id']:
             raise Http404
 
-        current_request = main_models.RegisterRequests.objects.get(id=int(data['id']))
-        if current_request is None:
+        current_request = main_models.RegisterRequests.objects.filter(id=int(data['id']))
+        if len(current_request) == 0:
             raise Http404
-
+        current_request = current_request[0]
         current_request.delete()
         return Response({"result": "deleted"})
 
 
 class LessonAPIView(APIView):
-    def get(self, request):
+    def get(self, request, lesson_id=None):
         if not request.user.is_authenticated:
             raise Http404
         if request.user.is_superuser:
+            if lesson_id is not None:
+                lesson = main_models.Lesson.objects.filter(id=lesson_id)
+                if len(lesson) == 0:
+                    raise Http404
+                return Response(LessonSerializer(lesson[0]).data)
             return Response(LessonSerializer(main_models.Lesson.objects.all(), many=True).data)
 
         teacher = main_models.Teacher.objects.filter(user=request.user)
@@ -87,5 +95,58 @@ class LessonAPIView(APIView):
             student = student[0]
             is_student = True
         if not is_student:
+            if lesson_id is not None:
+                lesson = main_models.Lesson.objects.filter(teacher=teacher, id=lesson_id)
+                if len(lesson):
+                    raise Http404
+                return Response(LessonSerializer(lesson[0]).data)
             return Response(LessonSerializer(main_models.Lesson.objects.filter(teacher=teacher), many=True).data)
+        if lesson_id is not None:
+            lesson = main_models.Lesson.objects.filter(group=student.group, id=lesson_id)
+            if len(lesson) == 0:
+                raise Http404
+            return Response(LessonSerializer(lesson[0]).data)
         return Response(LessonSerializer(main_models.Lesson.objects.filter(group=student.group), many=True).data)
+
+    def post(self, request):
+        if not request.user.is_superuser:
+            raise Http404
+        lesson = LessonSerializer(data=request.data)
+        if lesson.is_valid():
+            lesson.save()
+            return Response(lesson.validated_data)
+        return Response({'error': 'lesson is not valid'})
+
+    def put(self, request: HttpRequest, lesson_id=None):
+        data = request.data
+        lesson = main_models.Lesson.objects.filter(id=lesson_id)
+        if len(lesson) == 0:
+            raise Http404
+        lesson = lesson[0]
+
+        if request.user.is_superuser:
+            group = main_models.Group.objects.filter(id=int(data['group']))
+            teacher = main_models.Teacher.objects.filter(id=int(data['teacher']))
+            subject = main_models.Subject.objects.filter(id=int(data['subject']))
+            if len(group) == 0 or len(teacher) == 0 or len(subject) == 0:
+                return Response({'error': 'lesson is not valid'})
+            group = group[0]
+            teacher = teacher[0]
+            subject = subject[0]
+            lesson.group = group
+            lesson.subject = subject
+            lesson.teacher = teacher
+            lesson.date = data['date']
+            lesson.start_time = data['start_time']
+            lesson.end_time = data['end_time']
+            lesson.homework = data['homework']
+            lesson.room = data['room']
+            lesson.type = data['type']
+            lesson.save()
+            return Response(LessonSerializer(lesson).data)
+        if lesson.teacher.user == request.user:
+            lesson.homework = data['homework']
+            lesson.room = data['room']
+            lesson.save()
+            return Response(LessonSerializer(lesson).data)
+        raise Http404
