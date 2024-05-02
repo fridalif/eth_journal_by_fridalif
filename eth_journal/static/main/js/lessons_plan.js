17ec5e95f465a69cc1bca2cde5232f62c43bc9f9
fridalif <124243435+fridalif@users.noreply.github.com>
2024-05-02 23:33:46 +0300

    lessons view ended

--- eth_journal/static/main/js/lessons_plan.js
+++ eth_journal/static/main/js/lessons_plan.js
@@ -8,7 +8,18 @@ function get_lessons_from_date(date){
         side_bar_block = document.getElementById('marks_side_bar_content_block')
         side_bar_block.innerHTML = ''
         for (let i = 0; i < result.length; i++) {
-             result_info = result[i]['subject_name']
+             result_info = result[i]['subject_name'];
+             result_info += '(Кабинет: ' + result[i]['room']+', Тип: '+result[i]['type'];//+ 'Преподаватель:';
+             result_info +=', Начало: '+result[i]['start_time']+', Окончание: '+result[i]['end_time']+'';
+             teacher_info = ', Преподаватель: ';
+             if (result[i]['abstract_teacher_name']!=null){
+                teacher_info += result[i]['abstract_teacher_surname']+' '+result[i]['abstract_teacher_name']+' '+result[i]['abstract_teacher_father_name'];
+             }
+            else{
+                teacher_info += result[i]['teacher_surname']+' '+result[i]['teacher_name']+' '+result[i]['teacher_father_name'];
+            }
+            group_info = ', Группа: '+result[i]['group_year_of_study']+result[i]['group_letter'];
+            result_info+=teacher_info+group_info+')';
              side_bar_block.innerHTML += '<div class="marks_side_bar_lesson_block"><div class="marks_side_bar_font">'+result_info+'</div></div>';
         }
         return;
