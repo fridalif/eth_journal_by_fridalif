d68384dc120eabb991ad2cb5fd9ef3c82fc3a460
fridalif <124243435+fridalif@users.noreply.github.com>
2024-05-04 12:44:35 +0300

    header username

--- eth_journal/main/urls.py
+++ eth_journal/main/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views
 
 app_name = 'main'
 urlpatterns = [
-    path('index/', index, name='index'),
+    path('', index, name='index'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(next_page='/index/'), name='logout'),
     path('register/', register, name='register'),
