4e8462bdb2298c7be931028827c6a2f3af0c3eaa
fridalif <124243435+fridalif@users.noreply.github.com>
2024-05-07 15:50:43 +0300

    fixed errors in js

--- eth_journal/main/views.py
+++ eth_journal/main/views.py
@@ -139,5 +139,9 @@ def admin_requests_view(request: HttpRequest):
     if len(profile) == 0:
         raise Http404
     profile = profile[0]
-    context = {'user': request.user, 'my_profile': profile}
+    groups = Group.objects.all()
+    abstract_teachers = AbstractTeacher.objects.all()
+    abstract_students = AbstractKid.objects.all()
+    context = {'user': request.user, 'my_profile': profile, 'groups': groups, 'abstract_teachers': abstract_teachers,
+               'abstract_students': abstract_students}
     return render(request, 'main/admin_requests.html', context=context)
--- eth_journal/static/main/js/admin_requests.js
+++ eth_journal/static/main/js/admin_requests.js
@@ -1,48 +1,14 @@
-function get_all_abstract_kids(){
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET","/api/abstract_kids/");
-    xhr.responseType = 'json';
-    xhr.send();
-    xhr.onload = function(){
-        abstract_kids = xhr.response;
-        return;
-    }
-    return;
-}
-function get_all_abstract_teachers(){
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET","/api/abstract_teachers/");
-    xhr.responseType = 'json';
-    xhr.send();
-    xhr.onload = function(){
-        abstract_teachers = xhr.response;
-        return;
-    }
-    return;
-}
-
-function get_all_groups(){
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET","/api/groups/");
-    xhr.responseType = 'json';
-    xhr.send();
-    xhr.onload = function(){
-        groups = xhr.response;
-        return;
-    }
-    return;
-}
-
 function get_register_requests(){
     let xhr = new XMLHttpRequest();
     table = document.getElementById('admin_table_block')
     xhr.open("GET","/api/register_requests/");
     xhr.responseType = 'json';
     xhr.send();
+
     xhr.onload = function(){
         result = xhr.response;
         for(let row=0; row<result.length;row++){
-            row_div = '<div class="admin_table_row">';
+            row_div = '<div class="admin_table_row" id="row_'+row+'">';
             row_div += '<div class="admin_table_register_username_cell">'+result[row]['login']+'</div>';
             row_div += '<div class="admin_table_register_name_cell">'+result[row]['surname']+' '+result[row]['name']+' '+String(result[row]['father_name'])+'</div>';
             row_div += '<div class="admin_table_register_role_cell">'+result[row]['role']+'</div>';
@@ -78,12 +44,20 @@ function get_register_requests(){
             }
             row_div+='</select>';
             row_div +='</div>';
-            row_div +='<div class="admin_table_choose_accept" id="'+result[row]['id']+'">'+'</div>';
-            row_div +='<div class="admin_table_choose_deny" id="'+result[row]['id']+'">'+'</div>';
+            row_div +='<div class="admin_table_choose_accept" id="'+result[row]['id']+'" onclick="accept_register('+"'row_"+String(row)+"'"+');">✓'+'</div>';
+            row_div +='<div class="admin_table_choose_deny" id="'+result[row]['id']+'" onclick="deny_register('+"'row_"+String(row)+"'"+');">X'+'</div>';
             table.innerHTML+=row_div;
         }
         return;
     }
-    return;
+
 }
 
+function accept_register(row){
+    row_div = document.getElementById(String(row));
+    console.log(row_div);
+}
+function deny_register(row){
+    row_div = document.getElementById(String(row));
+    console.log(row_div);
+}
